2d8ba34a1dad1ca85e40221b223e9cb6054b0dc7
DoubleR0 <rewkung43@gmail.com>
2020-11-25 06:06:08 +0700

    last version purchase-service

--- src/main/java/purchase/service/Dataservice.java
+++ src/main/java/purchase/service/Dataservice.java
@@ -1,39 +1,123 @@
 package purchase.service;
 
 public class Dataservice {
-    private String id;
-    private String name;
-    private int    amount;
-
-    public Dataservice(String id, String name, int amount){
-        this.id = id;
-        this.name = name;
-        this.amount = amount;
+    private String purchaseidid;
+    private String cartid;
+    private String productid;
+    private String paymentAccountid;
+    private String paymentLogid;
+    private String userProfileid;
+    private double result;
+    private String address;
+    public Dataservice(String purchaseidid, String cartid, String productid,
+    String paymentAccountid, String paymentLogid, String userProfileid, double result, String address) {
+        this.purchaseidid = purchaseidid;
+        this.cartid = cartid;
+        this.productid = productid;
+        this.paymentAccountid = paymentAccountid;
+        this.paymentLogid = paymentLogid;
+        this.userProfileid = userProfileid;
+        this.result = result;
+        this.address = address;
     }
 
-    public String getId() {
-        return id;
+    public String getPurchaseidid() {
+        return purchaseidid;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setPurchaseidid(String purchaseidid) {
+        this.purchaseidid = purchaseidid;
     }
 
-    public String getName() {
-        return name;
+    public String getCartid() {
+        return cartid;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCartid(String cartid) {
+        this.cartid = cartid;
     }
 
-    public int getAmount() {
-        return amount;
+    public String getProductid() {
+        return productid;
     }
 
-    public void setAmount(int amount) {
-        this.amount = amount;
+    public void setProductid(String productid) {
+        this.productid = productid;
     }
+
+    public String getPaymentAccountid() {
+        return paymentAccountid;
+    }
+
+    public void setPaymentAccountid(String paymentAccountid) {
+        this.paymentAccountid = paymentAccountid;
+    }
+
+    public String getPaymentLogid() {
+        return paymentLogid;
+    }
+
+    public void setPaymentLogid(String paymentLogid) {
+        this.paymentLogid = paymentLogid;
+    }
+
+    public String getUserProfileid() {
+        return userProfileid;
+    }
+
+    public void setUserProfileid(String userProfileid) {
+        this.userProfileid = userProfileid;
+    }
+
+    public double getResultid() {
+        return result;
+    }
+
+    public void setResultid(double resultid) {
+        this.result = resultid;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    // private String name;
+    // private int    amount;
+
+    // public Dataservice(String id, String name, int amount){
+    //     this.id = id;
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    // public String getId() {
+    //     return id;
+    // }
+
+    // public void setId(String id) {
+    //     this.id = id;
+    // }
+
+    // public String getName() {
+    //     return name;
+    // }
+
+    // public void setName(String name) {
+    //     this.name = name;
+    // }
+
+    // public int getAmount() {
+    //     return amount;
+    // }
+
+    // public void setAmount(int amount) {
+    //     this.amount = amount;
+    // }
 
     
 }
--- src/main/java/purchase/service/ServiceController.java
+++ src/main/java/purchase/service/ServiceController.java
@@ -6,85 +6,37 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.*;
 
-import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class ServiceController {
-    List<Dataservice> dataservice = new ArrayList<>(Arrays.asList(new Dataservice("LIP001", "XOXO Lipstick", 1),
-            new Dataservice("BRO001", "NYX Brush On Palette", 1)));
+    List<Dataservice> dataPurchaseservice = new ArrayList<>(Arrays.asList(new Dataservice("1", "3", "1", "166929", "1000232", "pSFcMeuEtuY8PyVUdkYDtcCMCSi1", 9841.15, "3441 Coleman Avenue Escondido, CA 9202")));
 
     public void deleteData(String id) {
-        for (int i = 0; i < dataservice.size(); i++) {
-            if (dataservice.get(i).getId().equals(id)) {
-                dataservice.remove(i);
-                break;
-            }
-        }
+    for (int i = 0; i < dataPurchaseservice.size(); i++) {
+    if (dataPurchaseservice.get(i).getPurchaseidid().equals(id)) {
+        dataPurchaseservice.remove(i);
+    break;
     }
-
-    @RequestMapping(value = "/getAllData", method = RequestMethod.GET)
-    @ResponseBody
-    public List<Dataservice> getAllDataservice(HttpServletResponse response) {
-        return dataservice;
     }
-
-    @RequestMapping(value = "/getData{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public List<Dataservice> getDataservice(HttpServletRequest request, @PathVariable String id) {
-        return dataservice;
-    }
-
-    @RequestMapping(value = "/Data/{id}", method = RequestMethod.DELETE)
-    public String deleteDataservice(@PathVariable String id) {
-        deleteData(id);
-        return "Delete0";
-    }
-
-    @GetMapping("/hello")
-    @ResponseBody
-    public void index(HttpServletResponse response) throws IOException {
-        final String uri = "http://localhost:8080/getAllData";
-        RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
-        // JSONObject jsonObject = new JSONObject(result);
-
-        // System.out.println(jsonObject);
-        response.setContentType("application/json");
-        response.getWriter().print("{" + result + "," + result + "}");
-    }
-
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-            Model model) {
-        model.addAttribute("name", name);
-        // final String uri = "http://localhost:8080/getAllData";
-        // RestTemplate restTemplate = new RestTemplate();
-        // String result = restTemplate.getForObject(uri, String.class);
-
-        // System.out.println(result);
-        // return result;
-        return "greeting";
     }
 
     // PurchaseService
@@ -92,9 +44,9 @@ public class ServiceController {
     // set method and parameter
     @RequestMapping(value = "/confirmordered/{purchaseid}", method = RequestMethod.GET)
     @ResponseBody
-    public void confirmordered(HttpServletRequest request, HttpServletResponse response, @PathVariable String purchaseid) throws IOException {
-            // request sent to payment service
-        String obj = "{\"order\":\"154811629\"}";
+    public void confirmordered(HttpServletRequest request, HttpServletResponse response,
+            @PathVariable String purchaseid) throws IOException {
+        // request sent to payment service
         final String url = "http://localhost:8080/payment";
         RestTemplate restTemplate = new RestTemplate();
         // set header for request sent
@@ -102,7 +54,7 @@ public class ServiceController {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         // set Entity for request sent
-        HttpEntity<String> entity = new HttpEntity<>(obj, headers);
+        HttpEntity<String> entity = new HttpEntity<>(dataPurchaseservice.toString(), headers);
         // get response request sent
         ResponseEntity<String> respon = restTemplate.postForEntity(url, entity, String.class);
 
@@ -124,40 +76,18 @@ public class ServiceController {
     // set method and path
     @RequestMapping(value = "/detailorder/{orderid}", method = RequestMethod.GET)
     // @ResponseBody
-    public String detailorder(HttpServletRequest request, HttpServletResponse response, Model model, @PathVariable String orderid) throws IOException {
+    public String detailorder(HttpServletRequest request, HttpServletResponse response, Model model,
+            @PathVariable String orderid) throws IOException {
         String url = "https://order.tungmai.me/api/order/" + orderid;
         RestTemplate restTemplate = new RestTemplate();
         String dataservice = restTemplate.getForObject(url, String.class);
         JSONObject orderObject = new JSONObject(dataservice);
-        // get token from header request
-        // String authorization = request.getHeader("authorization");
-        // // get Jsonobject from body request
-        // String data = "";
-        // StringBuilder builder = new StringBuilder();
-        // BufferedReader reader = request.getReader();
-        // String line;
-        // while ((line = reader.readLine()) != null) {
-        //     builder.append(line);
-        // }
-        // data = builder.toString();
-        // // convert string to jsonobject
-        // JSONObject Body = new JSONObject(data);
-
-        // System.out.println("Body ====== " + Body);
-        // System.out.println("authorization ========= " + authorization);
-        // System.out.println(request);
-
-        // // request data from shiping
-        // String url = "http://localhost:8080/shiping";
-        // RestTemplate restTemplate = new RestTemplate();
-        // String dataservice = restTemplate.getForObject(url, String.class);
-        // JSONObject shipingObject = new JSONObject(dataservice);
 
         // request data from payment
-        // url = "https://sop-picnic.azurewebsites.net/profile/" + orderObject.get("user_id");
-        // restTemplate = new RestTemplate();
-        // dataservice = restTemplate.getForObject(url, String.class);
-        // JSONObject paymentObject = new JSONObject(dataservice);
+        url = "https://sop-picnic.azurewebsites.net/payment";
+        restTemplate = new RestTemplate();
+        dataservice = restTemplate.getForObject(url, String.class);
+        JSONObject paymentObject = new JSONObject(dataservice);
 
         // // request data from user
         url = "https://sop-picnic.azurewebsites.net/profile/" + orderObject.get("user_id");
@@ -167,15 +97,37 @@ public class ServiceController {
 
         // set responses type
         response.setContentType("application/json");
-        // set responses data
-        // System.out.println("shipingObject ======== " + shipingObject);
-        // response.getWriter().println(shipingObject);
-        // response.getWriter().println(paymentObject);
-        // response.getWriter().println(userObject);
-        // response.getWriter().println(orderObject);
-        // response.getWriter().println(orderObject.get("product"));
         // set responses code
+        System.out.println(orderObject.get("product").getClass());
         model.addAttribute("address", userObject.get("address"));
+
+        JSONArray jsonArray = new JSONArray(orderObject.get("product").toString());
+        JSONArray jsonPersonData = jsonArray;
+        System.out.print(jsonPersonData);
+        Integer id = 0;
+        for (int i=0; i<jsonPersonData.length(); i++) {
+            JSONObject item = jsonPersonData.getJSONObject(i);
+            // System.out.println(item);
+            id = (Integer) item.get("id");
+            model.addAttribute("amount", item.get("amount"));
+            model.addAttribute("total", item.get("total"));
+            // String name = item.getString("name");
+            // String surname = item.getString("surname");
+        }
+
+        url = "http://stock.phwt.me/product/" + id;
+        restTemplate = new RestTemplate();
+        dataservice = restTemplate.getForObject(url, String.class);
+        JSONObject productObject1 = new JSONObject(dataservice);
+
+        model.addAttribute("productObject1", productObject1.get("name"));
+        model.addAttribute("price1", orderObject.get(""));
+
+        url = "https://mhee-promotion.herokuapp.com/promotions";
+        restTemplate = new RestTemplate();
+        dataservice = restTemplate.getForObject(url, String.class);
+        JSONObject promotionsObject = new JSONObject(dataservice);
+        model.addAttribute("promotions", userObject.get("promotion"));
         response.setStatus(HttpServletResponse.SC_OK);
         return "first";
     }
@@ -185,13 +137,10 @@ public class ServiceController {
     @RequestMapping(value = "/log", method = RequestMethod.GET)
     @ResponseBody
     public void log(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-        // empty
-
         // set responses type
         response.setContentType("application/json");
         // set responses data
-        response.getWriter().print(response);
+        response.getWriter().print(dataPurchaseservice);
         // set responses code
         response.setStatus(HttpServletResponse.SC_OK);
     }
@@ -200,19 +149,18 @@ public class ServiceController {
     // set method and parameter
     @RequestMapping(value = "/log/{purchaseid}", method = RequestMethod.GET)
     @ResponseBody
-    public void logOne(HttpServletResponse response) throws IOException {
-
-        // empty
+    public void logOne(HttpServletRequest request, HttpServletResponse response, @PathVariable Integer purchaseid) throws IOException {
 
         // set responses type
         response.setContentType("application/json");
         // set responses data
-        response.getWriter().print(response);
+        response.getWriter().print(dataPurchaseservice.get(purchaseid));
         // set responses code
         response.setStatus(HttpServletResponse.SC_OK);
     }
 
-    // update payment from PaymentService and save to Dataservice, send data to ShipingService and send to OrderService to save history order
+    // update payment from PaymentService and save to Dataservice, send data to
+    // ShipingService and send to OrderService to save history order
     // set method and parameter
     @RequestMapping(value = "/updatepaymentlog/{purchaseid}", method = RequestMethod.PATCH)
     @ResponseBody
@@ -232,15 +180,14 @@ public class ServiceController {
         JSONObject Body = new JSONObject(data);
 
         // request sent to shiping service
-        String obj = "{\"order\":\"154811629\"}";
-        final String url = "http://localhost:8080/shiping";
+        final String url = "http://shipping-sop.herokuapp.com/shipping/";
         RestTemplate restTemplate = new RestTemplate();
         // set header for request sent
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         // set Entity for request sent
-        HttpEntity<String> entity = new HttpEntity<>(obj, headers);
+        HttpEntity<String> entity = new HttpEntity<>(dataPurchaseservice.toString(), headers);
         // get response request sent
         ResponseEntity<String> respon = restTemplate.postForEntity(url, entity, String.class);
 
@@ -260,14 +207,24 @@ public class ServiceController {
     // add Dataservice to log for emergency or maintenance
     @RequestMapping(value = "/addlog/{purchaseid}", method = RequestMethod.PUT)
     @ResponseBody
-    public void addlog(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void addlog(HttpServletRequest request, HttpServletResponse response) throws IOException, NumberFormatException, NullPointerException {
 
-        // empty
+        String data = "";
+        StringBuilder builder = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            builder.append(line);
+        }
+        data = builder.toString();
+        // convert string to jsonobject
+        JSONObject Body = new JSONObject(data);
+        List<Dataservice> dataPurchaseservice = new ArrayList<>(Arrays.asList(new Dataservice(Body.getJSONObject("purchaseidid").toString(), Body.getJSONObject("cartid").toString(), Body.getJSONObject("productid").toString(), Body.getJSONObject("paymentAccountid").toString(), Body.getJSONObject("paymentLogid").toString(), Body.getJSONObject("userProfileid").toString(), Double.parseDouble(Body.getJSONObject("result").toString()), Body.getJSONObject("address").toString())));
 
         // set responses type
         response.setContentType("application/json");
         // set responses data
-        response.getWriter().print(response);
+        response.getWriter().print(dataPurchaseservice);
         // set responses code
         response.setStatus(HttpServletResponse.SC_OK);
     }
@@ -275,14 +232,14 @@ public class ServiceController {
     // delete Dataservice from log when ordered finished or maintenance
     @RequestMapping(value = "/deletelog/{purchaseid}", method = RequestMethod.GET)
     @ResponseBody
-    public void deletelog(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void deletelog(HttpServletRequest request, HttpServletResponse response, @PathVariable String purchaseid) throws IOException {
 
-        // empty
+        deleteData(purchaseid);
 
         // set responses type
         response.setContentType("application/json");
         // set responses data
-        response.getWriter().print(response);
+        response.getWriter().print(dataPurchaseservice);
         // set responses code
         response.setStatus(HttpServletResponse.SC_OK);
     }
